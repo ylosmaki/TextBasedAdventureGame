c2ed9add4de2fb6d750d3e64c611b2aa6bf72113
yhimanen <y.himanen@gmail.com>
2020-01-29 10:06:53 +0200

    room descriptions all complete

--- src/main/java/HauntedHouse/LaundryRoom.java
+++ src/main/java/HauntedHouse/LaundryRoom.java
@@ -7,6 +7,8 @@ public class LaundryRoom extends Room {
 
     @Override
     public void introduceRoom() {
-        System.out.println("You have entered the laundry room.");
+        System.out.println("You have entered the laundry room. A pair of scissors lies on top of a rusty broken washing machine.\n" +
+                "A cabinet above the washing machine hangs slightly open. There seem to be some strange noises coming from the top-loading drier.\n" +
+                "You venture forward in search of keys...");
     }
 }
--- src/main/java/HauntedHouse/StorageRoom.java
+++ src/main/java/HauntedHouse/StorageRoom.java
@@ -6,6 +6,9 @@ public class StorageRoom extends Room {
 
     @Override
     public void introduceRoom() {
-        System.out.println("You trip over a box and fall into a cluttered storage room.");
+        System.out.println("You trip over a box and fall into a cluttered storage room.\n" +
+                "A broken flashlight and a wooden baseball bat lie abandoned in the corner next to a dusty old suitcase.\n" +
+                "There seem to be some papers sticking out of the corner of the suitcase. A pile of boxes has been lazily stacked in another corner.\n" +
+                "Well, those keys aren't going to find themselves...");
     }
 }
